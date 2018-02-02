72457fe835b6dc4d2da7de485ed0cc8bed76174f
黄芬 <fen.h@cn.micropoplar.com>
2018-02-02 15:59:20 +0800

    add search

---
+++ src/components/TeacherDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="detail-main">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "teacher-detail"
+  }
+</script>
+
+<style scoped>
+.detail-main{
+  margin-top: 5px;
+  width: 100%;
+  height: 159px;
+  background-color: #fff;
+  border-radius: 2px;
+}
+</style>
